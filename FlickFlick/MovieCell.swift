d6525d8cba6813a7a9d6b8a6e36979cf0f8cb35e
CheeryW <cheeryone821@gmail.com>
2019-02-13 00:20:41 -0800

    Homepage finished

---
+++ FlickFlick/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  FlickFlick
+//
+//  Created by Cheery Wang on 2/12/19.
+//  Copyright © 2019 Cheery Wang. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
